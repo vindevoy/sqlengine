853ab7ea7a3b60cf60da4f87dcb808d23498ad81
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-05-31 22:49:42 +0200

    #1 - Refactor EngineFactory class and update documentation
    The EngineFactory class in sqlengine/common/engine_factory.py has been refactored. Documentation has also been appended to shed more light on the purpose, methods and properties of the class. This included extensive comments and the addition of return type annotations to the get_engine method. Private properties have been preserved to maintain class encapsulation.

--- sqlengine/common/engine_factory.py
+++ sqlengine/common/engine_factory.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 
 import yaml
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 
 
 class EngineFactory:
+    """
+    The engine factory has only one purpose: to create an engine instance using the get_engine() method.
+    In that method the db_settings.yaml config file is read to retrieve the username, password, and database.
+    The class is implemented as a factory, meaning it uses only class methods.
+
+    :version: 1.0.0
+    :date: 2024-05-31
+    :author: Yves Vindevogel <yves@vindevogel.net>
+    """
     __settings = None
 
     __user_name: str = ""
@@ -13,11 +22,23 @@ class EngineFactory:
     __echo: bool = False
 
     @classmethod
-    def get_engine(cls):
+    def get_engine(cls) -> Engine:
+        """
+        This class method returns the SQLAlchemy engine instance, incorporating the username and password,
+        along the other database connection parameters.
+
+        :return: sqlalchemy.Engine
+            The SQLAlchemy engine instance.
+
+        :version: 1.0.0
+        :date: 2024-05-31
+        :author: Yves Vindevogel <yves@vindevogel.net>
+        """
         if cls.__settings is None:
             current_path = Path(__file__).parent
 
             with open(current_path.parent.joinpath("config", "db_settings.yaml"), "r") as db_file:
+                # All properties are private as only the get_engine method should be accessible.
                 cls.__settings = yaml.safe_load(db_file)
 
                 cls.__user_name = cls.__settings["user"]
