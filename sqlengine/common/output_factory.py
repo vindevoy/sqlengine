8afcfc35fab03ec28ac99e42d842855fcdab0db2
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-06-05 10:02:24 +0200

    #1 - Add OutputFactory for pretty printing and corresponding tests
    Created a new OutputFactory class responsible for pretty printing records in a tabular format. Also added new tests to verify the functionality of OutputFactory. This class helps to enhance readability by ensuring that "id" is always the first column and filtering out unnecessary object state fields.

---
+++ sqlengine/common/output_factory.py
@@ -0,0 +1,51 @@
+import pandas
+from tabulate import tabulate
+
+from sqlengine.models.base import Base
+
+
+class OutputFactory:
+    """
+    The engine factory has only one purpose: pretty print records in a tabular format.
+
+    :version: 1.0.0
+    :date: 2024-06-04
+    :author: Yves Vindevogel <yves@vindevogel.net>
+    """
+
+    @classmethod
+    def pretty_print(cls, data: list[Base]):
+        """
+        Print the records in a tabular format, "id" first, without any index indication.
+
+        :version: 1.0.0
+        :date: 2024-06-04
+        :author: Yves Vindevogel <yves@vindevogel.net>
+        """
+
+        if len(data) == 0:
+            print("\nNo data")
+
+        ###
+        # re-order the columns so that "id" is always the first.  Due to the inheritance of the base class,
+        # the "id" appears always as last column instead of the first.  That is not a problem in the class,
+        # but it looks a bit awkward.  This solves it.
+        columns = ["id"]
+        columns.extend(key for key in data[0].__table__.columns.keys() if key != "id")
+        ###
+
+        ###
+        # convert to a dataframe for this tabulate module.  However, when you take the __dict__,
+        # it also contains the object state in the fields.  Fortunately, this field begins with an underscore.
+        # We filter out the fields with an underscore.
+        df = pandas.DataFrame([row.__dict__ for row in data])
+        df = df.loc[:, ~df.columns.str.startswith('_')]
+        ###
+
+        ###
+        # re-order the columns in the dataframe according to our column order
+        df = df[columns]
+        ###
+
+        print("\n")  # This is for pytest.  If this is the first output, you need an extra line in pytest.
+        print(tabulate(df, headers=columns, showindex="never", tablefmt="fancy_outline"))
---
+++ tests/test_output.py
@@ -0,0 +1,26 @@
+from sqlengine.common.output_factory import OutputFactory
+from sqlengine.common.transaction_factory import TransactionFactory
+from sqlengine.db import drop_db, create_db
+from sqlengine.models.student import Student
+
+
+def setup_test():
+    drop_db.execute()
+    create_db.execute()
+
+
+def test_output():
+    setup_test()
+
+    assert Student.record_count() == 0
+
+    s1 = Student(first_name="Yves", last_name="Vindevogel", login="vindevoy")
+    s2 = Student(first_name="Niels", last_name="Vindevogel", login="vindevon")
+    s3 = Student(first_name="Next", last_name="Vindevogel", login="vindevox")
+
+    with TransactionFactory.get_transaction() as t:
+        t.create(s1, s2, s3)
+
+    result = Student.all()
+
+    OutputFactory.pretty_print(result)
