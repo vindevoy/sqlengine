808324255ee4bcc1e25e674466079e883c966e9b
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-06-05 12:06:43 +0200

    #1 - Add functionality to populate student data from CSV
    A new feature has been added where the student data is populated from a .csv file directly into the SQL database. This enhancement improves the database setup by automating the data input process, reducing the time and effort required to manually input the data. The feature uses Pandas for handling CSV files and adds new methods for populating the tables.

--- sqlengine/db/create_db.py
+++ sqlengine/db/create_db.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import pandas as pd
+
 from sqlengine.common.engine_factory import EngineFactory
 from sqlengine.models.student import Student
 
@@ -15,5 +19,34 @@ def execute() -> None:
     Student.metadata.create_all(engine)
 
 
-if __name__ == '__main__':
+def populate():
+    """
+    Populates the tables in the database based on the csv files.
+
+    :version: 1.0.0
+    :date: 2024-06-05
+    :author: Yves Vindevogel <yves@vindevogel.net>
+    """
+
+    __populate_table("tbl_students", "students.csv")
+
+
+def __populate_table(table_name: str, csv_file: str):
+    """
+    Populates a single table using pandas.
+
+    :version: 1.0.0
+    :date: 2024-06-05
+    :author: Yves Vindevogel <yves@vindevogel.net>
+    """
+
+    engine = EngineFactory.get_engine()
+    current_path = Path(__file__).parent
+
+    data = pd.read_csv(current_path.joinpath(csv_file), sep=";")
+    data.to_sql(table_name, engine, if_exists="replace", index=False)
+
+
+if __name__ == "__main__":
     execute()
+    populate()
